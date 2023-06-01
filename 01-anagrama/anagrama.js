0037a3d8a96534eb00919c0de01984c13ab6afc3
Camila Silva <camilasilvabernal18@gmail.com>
2023-06-01 16:27:09 -0500

    Termine la funcionalidad del boton para comprobar anagramas

--- 01-anagrama/anagrama.js
+++ 01-anagrama/anagrama.js
@@ -1,34 +1,28 @@
+const word1 = document.getElementById('input1');
+const word2 = document.getElementById('input2');
+const button = document.querySelector('#btn');
+const answer = document.querySelector('.answer');
 
-/* Reto #1
+button.addEventListener("click", anagrama)
 
-¿ES UN ANAGRAMA?}
 
-Dificultad: MEDIA
 
-Enunciado: Escribe una función que reciba dos palabras (String) y retorne verdadero o falso (Boolean) según sean o no anagramas.
-
-- Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
-- NO hace falta comprobar que ambas palabras existan.
-- Dos palabras exactamente iguales no son anagrama.
-
-*/
-
-let word1 = document.getElementById('input1').value;
-let word2 = document.getElementById('input2').value;
 
 function anagrama () {
-  if (word1.length === word2.length) {
-    const  letters1 = word1.split('').sort().toString ()
-    const  letters2 = word2.split('').sort().toString()
+  if (word1.length === word2.length && word1.value != word2.value) {
+    
+    const  letters1 = word1.value.split('').sort().toString ()
+    const  letters2 = word2.value.split('').sort().toString()
     
     if (letters1 == letters2) {
-      console.log('tus palabras son un anagrama')
+      const imAnagram = 'Son un anagrama 🤯'
+      answer.innerHTML = imAnagram
     } else {
-      console.log('Tus palabras NO son un Anagrama')
+      const notAnagram = 'Tus palabras NO son un Anagrama 🫠'
+      answer.innerHTML = notAnagram
     }
-
   } else {
-    console.log('Tus palabras NO son un Anagrama')
+    answer.innerHTML = 'Tus palabras son igual 😵‍💫'
   }
 }
-anagrama()
+
